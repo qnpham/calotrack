c7c0adb10edeb17e9639e9d10761ec4a6aaa10b0
qnpham <quannampham@gmail.com>
2026-04-03 21:22:15 -0700

    added auth

---
+++ app/auth/confirm/route.ts
@@ -0,0 +1,33 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const token_hash = requestUrl.searchParams.get("token_hash");
+  const type = requestUrl.searchParams.get("type");
+
+  if (token_hash && type) {
+    const cookieStore = await cookies();
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options),
+            );
+          },
+        },
+      },
+    );
+    await supabase.auth.verifyOtp({ token_hash, type: "magiclink" });
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,48 @@
+"use client";
 import CubeTransparent from "../../public/CubeTransparent.svg";
 import { FcGoogle } from "react-icons/fc";
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function loginPage() {
+export default function LoginPage() {
+  const [email, setEmail] = useState<string>("");
+
+  async function handleMagicLink() {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: "http://localhost:3000/auth/confirm",
+      },
+    });
+    if (error) console.error(error);
+    else alert("Check your email for the login link!");
+  }
+
   return (
     <div className="container max-w-lg mx-auto px-6">
       <div className="mt-70 ">
         <CubeTransparent className="w-18 mx-auto mb-6" />
         <h1 className="text-center font-semibold text-xl">CaloTrack AI</h1>
-        <div className="flex justify-center items- gap-2 border rounded-md py-2 mt-6 border-neutral-700">
-          <FcGoogle size={20} />
-          <span>Continue with Google</span>
+        <div className="mt-8">
+          <div className="flex gap-2">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="flex-1 bg-neutral-900 border border-neutral-700 rounded-lg p-2 text-white "
+              placeholder="Enter your email"
+            />
+            <button
+              className=" rounded-lg p-2 px-4 mx-auto bg-white text-black hover:pointer-cursor"
+              onClick={handleMagicLink}
+            >
+              Send link
+            </button>
+          </div>
+          <p className="text-center mt-4 text-neutral-500">
+            A login link will be sent to your email
+          </p>
         </div>
       </div>
     </div>
